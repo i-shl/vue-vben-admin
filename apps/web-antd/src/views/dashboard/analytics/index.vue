b5b4ae8b304970e7972c3d2e9063d1f25100258a
i-shl <1952066542@qq.com>
2025-11-25 15:12:10 +0800

    refactor: 更新组件规范，强制使用 defineOptions，添加模板根节点以防止页面空白

--- apps/web-antd/src/views/dashboard/analytics/index.vue
+++ apps/web-antd/src/views/dashboard/analytics/index.vue
@@ -46,30 +46,32 @@ const handleLogin = async () => {
 </script>
 
 <template>
-  <div class="p-5">这是分析页面</div>
-  <div class="p-5">打包后在dist目录执行http-server，接口正确</div>
-  <div class="p-5">
-    点击按钮调用登录接口的功能，默认显示请求数据，点击按钮后展现响应数据，响应数据需要显示在页面上。
-    <Button type="primary" :loading="loading" @click="handleLogin">
-      请求登录接口
-    </Button>
-  </div>
-  <div class="p-5">
-    <div class="mb-4">
-      <div class="mb-2 font-semibold">请求数据：</div>
-      <pre class="rounded bg-muted p-3 text-foreground">{{
-        JSON.stringify(requestData, null, 2)
-      }}</pre>
+  <div>
+    <div class="p-5">这是分析页面</div>
+    <div class="p-5">打包后在dist目录执行http-server，接口正确</div>
+    <div class="p-5">
+      点击按钮调用登录接口的功能，默认显示请求数据，点击按钮后展现响应数据，响应数据需要显示在页面上。
+      <Button type="primary" :loading="loading" @click="handleLogin">
+        请求登录接口
+      </Button>
     </div>
-    <div>
-      <div class="mb-2 font-semibold">响应数据：</div>
-      <div v-if="responseData" class="rounded bg-muted p-3">
-        <pre class="text-foreground">{{
-          JSON.stringify(responseData, null, 2)
+    <div class="p-5">
+      <div class="mb-4">
+        <div class="mb-2 font-semibold">请求数据：</div>
+        <pre class="rounded bg-muted p-3 text-foreground">{{
+          JSON.stringify(requestData, null, 2)
         }}</pre>
       </div>
-      <div v-else class="text-muted-foreground">
-        暂无响应数据，请点击按钮调用接口
+      <div>
+        <div class="mb-2 font-semibold">响应数据：</div>
+        <div v-if="responseData" class="rounded bg-muted p-3">
+          <pre class="text-foreground">{{
+            JSON.stringify(responseData, null, 2)
+          }}</pre>
+        </div>
+        <div v-else class="text-muted-foreground">
+          暂无响应数据，请点击按钮调用接口
+        </div>
       </div>
     </div>
   </div>
